32ab2ff2f977a2bc9d8172da424b581cefdd86d8
yash <166241872+yash5775@users.noreply.github.com>
2026-01-01 17:43:32 +0530

    feat: update `app/page.tsx` and regenerate development build artifacts

--- .next/dev/server/chunks/ssr/[root-of-the-server]__ce6aa389._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__ce6aa389._.js
@@ -115,7 +115,7 @@ function Home() {
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                 className: "absolute inset-0 flex items-center justify-center",
                                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                                    className: "relative w-32 h-32 md:w-48 md:h-48",
+                                    className: "relative w-48 h-48 md:w-72 md:h-72",
                                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {
                                         src: "/assets/logo.png",
                                         alt: "Anokhi Reet Logo",
--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
 
           {/* Central Logo - Fixed */}
           <div className="absolute inset-0 flex items-center justify-center">
-            <div className="relative w-32 h-32 md:w-48 md:h-48">
+            <div className="relative w-48 h-48 md:w-72 md:h-72">
               <Image
                 src="/assets/logo.png"
                 alt="Anokhi Reet Logo"
