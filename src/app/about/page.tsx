5317dd89c6ea65f890dba119f3d9bde2f4934844
yash <166241872+yash5775@users.noreply.github.com>
2026-02-04 12:40:13 +0530

    feat: Add title text and envelope images as overlays to the about page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,19 @@ export default function AboutPage() {
         >
             {/* Vector Image Overlay */}
             <div className="absolute top-0 left-0 w-full pointer-events-none flex justify-center pt-0">
-                <img src="/images/Vector.png" alt="" className="h-[100vmin] w-[100vmin] object-contain opacity-80 mix-blend-overlay" />
+                <div className="relative h-[100vmin] w-[100vmin]">
+                    <img src="/images/Vector.png" alt="" className="absolute inset-0 h-full w-full object-contain opacity-80 mix-blend-overlay" />
+
+                    {/* Title Text */}
+                    <div className="absolute inset-0 flex items-center justify-center z-10">
+                        <img src="/images/Group 29.png" alt="" className="w-[85%] object-contain" />
+                    </div>
+
+                    {/* Envelope */}
+                    <div className="absolute bottom-[10%] right-[5%] w-[45%] z-20">
+                        <img src="/images/Group 38.png" alt="" className="w-full object-contain" />
+                    </div>
+                </div>
             </div>
 
             {/* Intentionally blank as requested */}
